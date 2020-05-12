5c9407a2076715753dcad4c47f7befd516b234b7
Wasfi Al-Quran <65094883+WasfiAl-Quran@users.noreply.github.com>
2020-05-12 08:26:01 +0200

    Update PasswordVerifierTest.java

--- QA_Code_HW/src/PasswordVerifier/PasswordVerifierTest.java
+++ QA_Code_HW/src/PasswordVerifier/PasswordVerifierTest.java
@@ -1,9 +1,9 @@
 package PasswordVerifier;
 
 import org.junit.Test;
-import static org.junit.Assert.assertTrue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class PasswordVerifierTest {
 
@@ -11,121 +11,181 @@ public class PasswordVerifierTest {
 
     @Test
     public void passwordShouldBeLargerThan8Chars(){
-        PasswordVerifier.Verify("aAAA11a");
-        assertThat(PasswordVerifier.logHand.getLog(), containsString("password should be larger than 8 chars"));
+        IllegalArgumentException thrown = assertThrows(
+                IllegalArgumentException.class,
+                () -> PasswordVerifier.Verify("aAAA11a")
+        );
+        assertThat(thrown.getMessage(), containsString("password should be larger than 8 chars"));
     }
 
     @Test
     public void passwordShouldNotBeNull(){
-        PasswordVerifier.Verify("");
-        assertThat(PasswordVerifier.logHand.getLog(), containsString("password should not be null"));
+        IllegalArgumentException thrown = assertThrows(
+                IllegalArgumentException.class,
+                () -> PasswordVerifier.Verify("")
+        );
+        assertThat(thrown.getMessage(), containsString("password should not be null"));
     }
 
     @Test
     public void passwordShouldHaveOneUppercaseLetterAtLeast(){
-        PasswordVerifier.Verify("aaaaaa11a");
-        assertThat(PasswordVerifier.logHand.getLog(), containsString("password should have one uppercase letter at least"));
+        IllegalArgumentException thrown = assertThrows(
+                IllegalArgumentException.class,
+                () -> PasswordVerifier.Verify("aaaaaa11a")
+        );
+        assertThat(thrown.getMessage(), containsString("password should have one uppercase letter at least"));
     }
 
     @Test
     public void passwordShouldHaveOneLowercaseLetterAtLeast(){
-        PasswordVerifier.Verify("AAAAAAAA");
-        assertThat(PasswordVerifier.logHand.getLog(), containsString("password should have one lowercase letter at least"));
+        IllegalArgumentException thrown = assertThrows(
+                IllegalArgumentException.class,
+                () -> PasswordVerifier.Verify("AAAAAAAA")
+        );
+        assertThat(thrown.getMessage(), containsString("password should have one lowercase letter at least"));
     }
 
     @Test
     public void passwordShouldHaveOneNumberAtLeast(){
-        PasswordVerifier.Verify("aAAAAAAAa");
-        assertThat(PasswordVerifier.logHand.getLog(), containsString("password should have one number at least"));
+        IllegalArgumentException thrown = assertThrows(
+                IllegalArgumentException.class,
+                () -> PasswordVerifier.Verify("aAAAAAAAa")
+        );
+        assertThat(thrown.getMessage(), containsString("password should have one number at least"));
     }
 
     @Test
     public void ifThreeConditions_areTrue_return_PasswordIsOK_case1(){
-        PasswordVerifier.Verify("8888888Cs");
-        assertTrue(PasswordVerifier.is_PasswordOK ().contains("Password is OK"));
+        IllegalArgumentException thrown = assertThrows(
+                IllegalArgumentException.class,
+                () -> PasswordVerifier.Verify("8888888Cs")
+        );
+        assertThat(thrown.getMessage(), containsString("Password is OK"));
     }
 
     @Test
     public void ifThreeConditions_areTrue_return_PasswordIsOK_case2(){
-        PasswordVerifier.Verify("8888888C1");
-        assertTrue(PasswordVerifier.is_PasswordOK ().contains("Password is OK"));
+        IllegalArgumentException thrown = assertThrows(
+                IllegalArgumentException.class,
+                () -> PasswordVerifier.Verify("8888888s1")
+        );
+        assertThat(thrown.getMessage(), containsString("Password is OK"));
     }
 
     @Test
     public void ifThreeConditions_areTrue_return_PasswordIsOK_case3(){
-        PasswordVerifier.Verify("8888888s1");
-        assertTrue(PasswordVerifier.is_PasswordOK ().contains("Password is OK"));
+        IllegalArgumentException thrown = assertThrows(
+                IllegalArgumentException.class,
+                () -> PasswordVerifier.Verify("CCCCCCCCs")
+        );
+        assertThat(thrown.getMessage(), containsString("Password is OK"));
     }
 
     @Test
     public void ifThreeConditions_areTrue_return_PasswordIsOK_case4(){
-        PasswordVerifier.Verify("CCCCCCCCs");
-        assertTrue(PasswordVerifier.is_PasswordOK ().contains("Password is OK"));
+        IllegalArgumentException thrown = assertThrows(
+                IllegalArgumentException.class,
+                () -> PasswordVerifier.Verify("88888888s")
+        );
+        assertThat(thrown.getMessage(), containsString("Password is OK"));
     }
 
     @Test
     public void ifThreeConditions_areTrue_return_PasswordIsOK_case5(){
-        PasswordVerifier.Verify("88888888C");
-        assertTrue(PasswordVerifier.is_PasswordOK ().contains("Password is OK"));
+        IllegalArgumentException thrown = assertThrows(
+                IllegalArgumentException.class,
+                () -> PasswordVerifier.Verify("sssssssss")
+        );
+        assertThat(thrown.getMessage(), containsString("Password is OK"));
     }
 
     @Test
     public void ifThreeConditions_areTrue_return_PasswordIsOK_case6(){
-        PasswordVerifier.Verify("88888888s");
-        assertTrue(PasswordVerifier.is_PasswordOK ().contains("Password is OK"));
+        IllegalArgumentException thrown = assertThrows(
+                IllegalArgumentException.class,
+                () -> PasswordVerifier.Verify("Cs1")
+        );
+        assertThat(thrown.getMessage(), containsString("Password is OK"));
     }
 
     @Test
     public void ifThreeConditions_areTrue_return_PasswordIsOK_case7(){
-        PasswordVerifier.Verify("888888881");
-        assertTrue(PasswordVerifier.is_PasswordOK ().contains("Password is OK"));
+        IllegalArgumentException thrown = assertThrows(
+                IllegalArgumentException.class,
+                () -> PasswordVerifier.Verify("Cs")
+        );
+        assertThat(thrown.getMessage(), containsString("Password is OK"));
     }
 
     @Test
     public void ifThreeConditions_areTrue_return_PasswordIsOK_case8(){
-        PasswordVerifier.Verify("888888888");
-        assertTrue(PasswordVerifier.is_PasswordOK ().contains("Password is OK"));
+        IllegalArgumentException thrown = assertThrows(
+                IllegalArgumentException.class,
+                () -> PasswordVerifier.Verify("s1")
+        );
+        assertThat(thrown.getMessage(), containsString("Password is OK"));
     }
 
     @Test
-    public void ifThreeConditions_areTrue_return_PasswordIsOK_case9(){
-        PasswordVerifier.Verify("CCCCCCCCC");
-        assertTrue(PasswordVerifier.is_PasswordOK ().contains("Password is OK"));
+    public void ifNotOneLowercaseLetterAtLeast_return_PasswordIsNeverOK_case1(){
+        IllegalArgumentException thrown = assertThrows(
+                IllegalArgumentException.class,
+                () -> PasswordVerifier.Verify("AAAAA11A")
+        );
+        assertThat(thrown.getMessage(), containsString("Password is never OK"));
     }
 
     @Test
-    public void ifThreeConditions_areTrue_return_PasswordIsOK_case10(){
-        PasswordVerifier.Verify("sssssssss");
-        assertTrue(PasswordVerifier.is_PasswordOK ().contains("Password is OK"));
+    public void ifThreeConditions_areTrue_return_PasswordIsNeverOK_case2(){
+        IllegalArgumentException thrown = assertThrows(
+                IllegalArgumentException.class,
+                () -> PasswordVerifier.Verify("88888888C")
+        );
+        assertThat(thrown.getMessage(), containsString("Password is never OK"));
     }
 
     @Test
-    public void ifThreeConditions_areTrue_return_PasswordIsOK_case11(){
-        PasswordVerifier.Verify("Cs1");
-        assertTrue(PasswordVerifier.is_PasswordOK ().contains("Password is OK"));
+    public void ifThreeConditions_areTrue_return_PasswordIsNeverOK_case3(){
+        IllegalArgumentException thrown = assertThrows(
+                IllegalArgumentException.class,
+                () -> PasswordVerifier.Verify("C1")
+        );
+        assertThat(thrown.getMessage(), containsString("Password is never OK"));
     }
 
     @Test
-    public void ifThreeConditions_areTrue_return_PasswordIsOK_case12(){
-        PasswordVerifier.Verify("Cs");
-        assertTrue(PasswordVerifier.is_PasswordOK ().contains("Password is OK"));
+    public void ifThreeConditions_areTrue_return_PasswordIsNeverOK_case4(){
+        IllegalArgumentException thrown = assertThrows(
+                IllegalArgumentException.class,
+                () -> PasswordVerifier.Verify("888888881")
+        );
+        assertThat(thrown.getMessage(), containsString("Password is never OK"));
     }
 
     @Test
-    public void ifThreeConditions_areTrue_return_PasswordIsOK_case13(){
-        PasswordVerifier.Verify("C1");
-        assertTrue(PasswordVerifier.is_PasswordOK ().contains("Password is OK"));
+    public void ifThreeConditions_areTrue_return_PasswordIsNeverOK_case5(){
+        IllegalArgumentException thrown = assertThrows(
+                IllegalArgumentException.class,
+                () -> PasswordVerifier.Verify("888888888")
+        );
+        assertThat(thrown.getMessage(), containsString("Password is never OK"));
     }
 
     @Test
-    public void ifThreeConditions_areTrue_return_PasswordIsOK_case14(){
-        PasswordVerifier.Verify("s1");
-        assertTrue(PasswordVerifier.is_PasswordOK ().contains("Password is OK"));
+    public void ifThreeConditions_areTrue_return_PasswordIsNeverOK_case6(){
+        IllegalArgumentException thrown = assertThrows(
+                IllegalArgumentException.class,
+                () -> PasswordVerifier.Verify("CCCCCCCCC")
+        );
+        assertThat(thrown.getMessage(), containsString("Password is never OK"));
     }
 
     @Test
-    public void ifNotOneLowercaseLetterAtLeast_return_PasswordIsNeverOK(){
-        PasswordVerifier.Verify("AAAAA11A");
-        assertTrue(PasswordVerifier.is_PasswordNeverOK ().contains("Password is never OK"));
+    public void ifThreeConditions_areTrue_PasswordIsNeverOK_case7(){
+        IllegalArgumentException thrown = assertThrows(
+                IllegalArgumentException.class,
+                () -> PasswordVerifier.Verify("8888888C1")
+        );
+        assertThat(thrown.getMessage(), containsString("Password is never OK"));
     }
 }
